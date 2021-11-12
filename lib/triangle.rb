a79daf6c35605421c799b77fd88929a370b64d35
D-Justice <justice.damian96@gmail.com>
2021-11-12 15:47:23 +1100

    Assignment Complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validation
+    if a == b && b == c
+      return :equilateral
+    elsif a == b || b == c || c == a 
+      return :isosceles
+    else a !=b && b != c && c != a
+      return :scalene
+    end
+  end
+  def validation
+    [a,b,c].each do |value|
+      if value <= 0 || (a + b) <= c || (c + b) <= a || (a + c) <= b
+        throw_error
+      end
+    end
+  end
+  def throw_error
+    raise TriangleError
+
+  end
+  class TriangleError < StandardError
+  end
 end
